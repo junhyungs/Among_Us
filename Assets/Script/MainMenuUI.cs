0e43e0a0771871730b9141abd3f3598652c2e65b
junhyungs <jin05003@naver.com>
2025-01-24 17:08:08 +0900

    [feat] MainMenuUI
    에디터 종료코드 작성, 온라인 버튼 클릭 이벤트 함수 작성.

---
+++ Assets/Script/MainMenuUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void OnClickOnlineButton()
+    {
+        Debug.Log("OnClickOnlineButton");
+    }
+
+    public void OnClickQuitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
