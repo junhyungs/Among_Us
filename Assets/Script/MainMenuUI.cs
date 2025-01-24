9e84dd5f46fac1aea0c2119002bf5d5f0528369e
junhyungs <jin05003@naver.com>
2025-01-24 19:08:47 +0900

    [feat] 크루원 스프라이트 추가

--- Assets/Script/MainMenuUI.cs
+++ Assets/Script/MainMenuUI.cs
@@ -10,7 +10,7 @@ public class MainMenuUI : MonoBehaviour
     public void OnClickQuitButton()
     {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false; //에디터 종료
 #else
         Application.Quit();
 #endif
